43a085906d54739795deb3e6ba1196bd89472e8d
Arjun Gore <arjun.c.gore@gmail.com>
2026-05-13 01:47:55 -0400

    Fixes by Professor Tsang

--- src/fsx492.c
+++ src/fsx492.c
@@ -158,9 +158,9 @@ static inline int clear_blks(uint32_t start, uint32_t n)
  * @return     0        if block is allocated
  *             -EINVAL  if block is not allocated
  */
-static inline int validate_block(uint32_t blkno, struct context *ctx)
+static inline int validate_block(uint32_t blkno, struct context * ctx)
 {
-    return !FD_ISSET(blkno, ctx->block_map) ? -EINVAL : 0;
+    return (!blkno || !FD_ISSET(blkno, ctx->block_map)) ? -EINVAL : 0;
 }
 
 /**
@@ -235,9 +235,9 @@ static inline size_t count_avail_blks(struct context *ctx)
  * @return     0        inode exists
  *             -EINVAL  inode does not exist or invalid
  */
-static inline int validate_inode(uint32_t ino, struct context *ctx)
+static inline int validate_inode(uint32_t ino, struct context * ctx)
 {
-    return !FD_ISSET(ino, ctx->inode_map) ? -EINVAL : 0;
+    return (!ino || !FD_ISSET(ino, ctx->inode_map)) ? -EINVAL : 0;
 }
 
 /**
